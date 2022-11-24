072b6168aac0dc4dba574fd3e3ccfb1025845189
Prajwal Nadagouda <32608138+prajwalnadagouda@users.noreply.github.com>
2022-11-23 23:08:49 -0800

    making changes to database

--- db/init.sql
+++ db/init.sql
@@ -2,5 +2,5 @@ CREATE DATABASE travel;
 
 use travel;
 
-CREATE TABLE events (event_id INT PRIMARY KEY, event_date DATE, event_venue VARCHAR(255), event_lat INT,event_lon INT, event_lowest_price INT, event_highest_price INT);
+CREATE TABLE events (event_id INT PRIMARY KEY, event_date DATE, event_venue VARCHAR(255), event_lat FLOAT(10, 6),event_lon FLOAT(10, 6), event_lowest_price INT, event_highest_price INT);
 
